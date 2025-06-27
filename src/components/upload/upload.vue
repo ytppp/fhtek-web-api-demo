d30597b488e74dbea7d845fa7740ff94bd1e5478
ytppp <651003708@qq.com>
2025-06-27 17:09:12 +0800

    fix test issues

--- src/components/upload/upload.vue
+++ src/components/upload/upload.vue
@@ -162,9 +162,7 @@ export default {
   },
   computed: {
     uploadDisabled() {
-      return (
-        this.disabled || (this.form || {}).disabled.value || this.uploadLoading || this.uploadFail
-      )
+      return this.disabled || (this.form || {}).disabled.value || this.uploadLoading
     },
     uploadSuccess() {
       return this.status === UploadStatus.success
