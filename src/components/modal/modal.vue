343de3ddfa400006166f9af07a4bd9dde46be117
ytppp <651003708@qq.com>
2025-07-04 15:03:49 +0800

    update log

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -34,7 +34,7 @@
 </template>
 
 <script setup>
-import { ref, useSlots, watch } from 'vue'
+import { ref, useSlots } from 'vue'
 import FhPopup from '@/components/popup/popup.vue'
 
 defineOptions({
