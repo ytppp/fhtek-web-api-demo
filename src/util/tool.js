4e94e4d86b31a40ee64c6b1b0538dbfa73fe42df
ytppp <651003708@qq.com>
2025-09-29 17:13:22 +0800

    fix test issues

--- src/util/tool.js
+++ src/util/tool.js
@@ -264,21 +264,6 @@ export function isValidMask(ip) {
     return false
   }
   // 有效mask
-  if (result[0].includes('0') || result[1].includes('1')) {
-    return false
-  }
-  return true
-}
-
-export function isValidMaskSpecial(ip) {
-  if (ip.split('.').filter((val) => val).length !== 4) return false
-  const i = ip2int(ip).toString(2).padStart(32, '0')
-  const result = i.split('10')
-  // result.length !== 2
-  if (result.length > 2) {
-    return false
-  }
-  // 有效mask
   if (result[0].includes('0') || (result[1] && result[1].includes('1'))) {
     return false
   }
