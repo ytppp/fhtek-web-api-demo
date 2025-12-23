bb97489d44bde6e8c0de7c2868cf3f7b644a4f87
ytppp <651003708@qq.com>
2025-12-23 11:23:48 +0800

    fix test issues

--- src/pages/security/ipv4-filter.vue
+++ src/pages/security/ipv4-filter.vue
@@ -530,6 +530,7 @@ export default {
         : currentData.filter((item) => item.index !== modalForm.index)
 
       let flag = true
+      const allItem = tempData.find((item) => item.proto === ProtocolType.ALL)
       /*
        * 如果新增是ALL协议，判断列表中是否已经有ALL协议项
        *  如果有，判断两者src_ip、dest_ip、dest_port是否完全一致，一致则不允许新增
@@ -539,13 +540,7 @@ export default {
        *  如果没有，判断列表中协议项与新增项proto、src_ip、dest_ip、dest_port是否完全一致，一致则不允许新增
        */
       if (modalForm.proto === ProtocolType.ALL) {
-        const allItem = tempData.find((item) => item.proto === ProtocolType.ALL)
-        if (allItem) {
-          flag =
-            modalForm.src_ip !== allItem.src_ip ||
-            modalForm.dest_ip !== allItem.dest_ip ||
-            modalForm.dest_port !== allItem.dest_port
-        } else {
+        if (!allItem) {
           flag = !tempData.some(
             (item) =>
               modalForm.src_ip === item.src_ip &&
@@ -554,13 +549,7 @@ export default {
           )
         }
       } else {
-        const allItem = tempData.find((item) => item.proto === ProtocolType.ALL)
-        if (allItem) {
-          flag =
-            modalForm.src_ip !== allItem.src_ip ||
-            modalForm.dest_ip !== allItem.dest_ip ||
-            modalForm.dest_port !== allItem.dest_port
-        } else {
+        if (!allItem) {
           flag = !tempData.some(
             (item) =>
               item.proto === modalForm.proto &&
@@ -569,6 +558,12 @@ export default {
               modalForm.dest_port === item.dest_port,
           )
         }
+      }
+      if (allItem) {
+        flag =
+          modalForm.src_ip !== allItem.src_ip ||
+          modalForm.dest_ip !== allItem.dest_ip ||
+          modalForm.dest_port !== allItem.dest_port
       }
 
       if (!flag) {
